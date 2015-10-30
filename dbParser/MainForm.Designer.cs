771665c8eb7162c593088ed5d34840a1d8fbf6ad
Кучукбаев Руслан Рафисович <goidokodaka@gmail.com>
2015-10-30 04:26:57 +0300

    Repo creation

---
+++ dbParser/MainForm.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace dbParser
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RecordsProgressBar = new System.Windows.Forms.ProgressBar();
+            this.RecordsLabel = new System.Windows.Forms.Label();
+            this.SPCButton = new System.Windows.Forms.Button();
+            this.SpeedMeasureLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RecordsProgressBar
+            // 
+            this.RecordsProgressBar.Location = new System.Drawing.Point(12, 12);
+            this.RecordsProgressBar.Name = "RecordsProgressBar";
+            this.RecordsProgressBar.Size = new System.Drawing.Size(339, 35);
+            this.RecordsProgressBar.Step = 1;
+            this.RecordsProgressBar.TabIndex = 0;
+            // 
+            // RecordsLabel
+            // 
+            this.RecordsLabel.AutoSize = true;
+            this.RecordsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RecordsLabel.Location = new System.Drawing.Point(177, 63);
+            this.RecordsLabel.Name = "RecordsLabel";
+            this.RecordsLabel.Size = new System.Drawing.Size(18, 20);
+            this.RecordsLabel.TabIndex = 1;
+            this.RecordsLabel.Text = "0";
+            // 
+            // SPCButton
+            // 
+            this.SPCButton.Location = new System.Drawing.Point(94, 119);
+            this.SPCButton.Name = "SPCButton";
+            this.SPCButton.Size = new System.Drawing.Size(192, 31);
+            this.SPCButton.TabIndex = 2;
+            this.SPCButton.Text = "Start";
+            this.SPCButton.UseVisualStyleBackColor = true;
+            this.SPCButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // SpeedMeasureLabel
+            // 
+            this.SpeedMeasureLabel.AutoSize = true;
+            this.SpeedMeasureLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SpeedMeasureLabel.Location = new System.Drawing.Point(116, 96);
+            this.SpeedMeasureLabel.Name = "SpeedMeasureLabel";
+            this.SpeedMeasureLabel.Size = new System.Drawing.Size(154, 20);
+            this.SpeedMeasureLabel.TabIndex = 3;
+            this.SpeedMeasureLabel.Text = "0 records per minute";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 181);
+            this.Controls.Add(this.SpeedMeasureLabel);
+            this.Controls.Add(this.SPCButton);
+            this.Controls.Add(this.RecordsLabel);
+            this.Controls.Add(this.RecordsProgressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Parser";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar RecordsProgressBar;
+        private System.Windows.Forms.Label RecordsLabel;
+        private System.Windows.Forms.Button SPCButton;
+        private System.Windows.Forms.Label SpeedMeasureLabel;
+    }
+}
+
---
+++ dbParser/MainForm.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+using Npgsql;
+
+namespace dbParser
+{
+    public partial class MainForm : Form
+    {
+        private struct RecordBounds
+        {
+            public readonly int Begin;
+            public readonly int End;
+            public readonly int ThreadNumber;
+
+            public RecordBounds(int begin, int end, int threadNumber)
+            {
+                Begin = begin;
+                End = end;
+                ThreadNumber = threadNumber;
+            }
+        }
+
+        private const int RecordsMax = 3795666;
+        private const int ThreadsNumber = 5;
+        private int _threadsCounter; 
+        private int _count;
+        private bool _close;
+
+        private readonly StreamWriter _outputFile;
+
+        private readonly DbConnection _dbConnection;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            RecordsProgressBar.Maximum = RecordsMax;
+            _count = 0;
+            _lockerBegin = new object();
+            _lockerEnd = new object();
+            _close = false;
+            _threadsCounter = 0;
+
+            _outputFile = new StreamWriter("output.txt");
+            _lockerFile = new object();
+
+            DbConnectionStringBuilder connectionStringBuilder = new NpgsqlConnectionStringBuilder();
+            connectionStringBuilder.Add("Server", "localhost");
+            connectionStringBuilder.Add("Port", "5432");
+            connectionStringBuilder.Add("User Id", "test");
+            connectionStringBuilder.Add("Password", "1234");
+            connectionStringBuilder.Add("Database", "project");
+            _dbConnection = new NpgsqlConnection(connectionStringBuilder.ToString());
+            _dbConnection.Open();
+        }
+
+        private void StartButton_Click(object sender, EventArgs eargs)
+        {
+            const int part = RecordsMax/ThreadsNumber;
+            var begin = 1;
+            var end = part;
+            for (var i = 0; i < ThreadsNumber; i++)
+            {
+                var b = begin;
+                var e = end;
+                var index = i;
+                ThreadPool.QueueUserWorkItem(Parse, new RecordBounds(b, e, index + 1));
+                begin += part;
+                end += part;
+            }
+            ThreadPool.QueueUserWorkItem(MeasureTime);
+        }
+
+        private readonly object _lockerBegin;
+        private readonly object _lockerEnd;
+
+        private readonly object _lockerFile;
+
+        private void MeasureTime(object stateInfo)
+        {
+            Interlocked.Increment(ref _threadsCounter);
+            while (!_close)
+            {
+                var previousValue = _count;
+                Thread.CurrentThread.Join(2000);
+                var currentValue = _count;
+                if (SpeedMeasureLabel.InvokeRequired)
+                {
+                    SpeedMeasureLabel.Invoke((MethodInvoker) delegate
+                    {
+                        SpeedMeasureLabel.Text = $"{((currentValue - previousValue) / (float)2) * 60} records per minute";
+                    });
+                }
+            }
+            Interlocked.Decrement(ref _threadsCounter);
+        }
+
+        private void Parse(object bounds)
+        {
+            if (!(bounds is RecordBounds))
+                throw new ApplicationException();
+            Interlocked.Increment(ref _threadsCounter);
+            var recordBounds = (RecordBounds)bounds;
+            //lock (_lockerBegin)
+            //{
+            //    MessageBox.Show($"Thread {recordBounds.ThreadNumber} has started!");
+            //}
+            var i = recordBounds.Begin;
+            while (i < recordBounds.End && !_close)
+            {
+                try
+                {
+                    var xml = new StreamReader(
+                        // ReSharper disable once AssignNullToNotNullAttribute
+                        WebRequest.Create($"http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?an={i}")
+                            .GetResponse()
+                            .GetResponseStream()).ReadToEnd();
+                    var xmlReader = XmlReader.Create(new StringReader(xml));
+                    
+                    if (!xmlReader.ReadToFollowing("Error"))
+                    {
+                        xmlReader.Close();
+
+                        Interlocked.Increment(ref _count);
+
+                        string title = string.Empty,
+                            authors = string.Empty,
+                            pubtitle = string.Empty,
+                            pubtype = string.Empty,
+                            publisher = string.Empty,
+                            description = string.Empty,
+                            mdurl = string.Empty,
+                            pdf = string.Empty;
+
+                        List<string> controlledTerms = new List<string>(),
+                            uncontrolledTerms = new List<string>();
+
+                        var xmlDoc = new XmlDocument();
+                        xmlDoc.LoadXml(xml);
+                        XmlNode node;
+
+                        node = xmlDoc.SelectSingleNode("//root/document/title");
+                        if (node != null)
+                            title = node.InnerText;
+
+                        node = xmlDoc.SelectSingleNode("//root/document/authors");
+                        if (node != null)
+                            authors = node.InnerText;
+
+                        node = xmlDoc.SelectSingleNode("//root/document/pubtitle");
+                        if (node != null)
+                            pubtitle = node.InnerText;
+
+                        node = xmlDoc.SelectSingleNode("//root/document/pubtype");
+                        if (node != null)
+                            pubtype = node.InnerText;
+
+                        node = xmlDoc.SelectSingleNode("//root/document/publisher");
+                        if (node != null)
+                            publisher = node.InnerText;
+
+                        node = xmlDoc.SelectSingleNode("//root/document/abstract");
+                        if (node != null)
+                            description = node.InnerText;
+
+                        node = xmlDoc.SelectSingleNode("//root/document/mdurl");
+                        if (node != null)
+                            mdurl = node.InnerText;
+
+                        node = xmlDoc.SelectSingleNode("//root/document/pdf");
+                        if (node != null)
+                            pdf = node.InnerText;
+
+                        XmlNodeList xmlNodesList;
+
+                        xmlNodesList = xmlDoc.SelectNodes("//root/document/controlledterms");
+                        if (xmlNodesList != null)
+                        {
+                            controlledTerms.AddRange(from XmlNode xmlNode in xmlNodesList
+                                select xmlNode.ChildNodes
+                                into childNodes
+                                from XmlNode childNode in childNodes
+                                select childNode.InnerText);
+                        }
+
+                        xmlNodesList = xmlDoc.SelectNodes("//root/document/uncontrolledterms");
+                        if (xmlNodesList != null)
+                        {
+                            uncontrolledTerms.AddRange(from XmlNode xmlNode in xmlNodesList
+                                select xmlNode.ChildNodes
+                                into childNodes
+                                from XmlNode childNode in childNodes
+                                select childNode.InnerText);
+                        }
+
+
+                        string controlledTermsString = "{" + controlledTerms.Aggregate("", (aggr, str) => aggr + "[" + str + "]") + "}",
+                            uncontrolledTermsString = "{" + uncontrolledTerms.Aggregate("", (aggr, str) => aggr + "[" + str + "]") + "}";
+
+                        lock (_lockerFile)
+                        {
+                            _outputFile.WriteLine(
+                                $"title: {title} authors: {authors} pubtitle: {pubtitle} pubtype: {pubtype} " +
+                                $"publisher: {publisher} description: {description} mdurl: {mdurl} pdf: {pdf} " +
+                                $"controlledterms: {controlledTermsString} uncontrolledterms: {uncontrolledTermsString}");
+                            _outputFile.Flush();
+                        }
+
+                        RecordsLabel.Invoke((MethodInvoker)delegate
+                        {
+                            RecordsLabel.Text = $"{_count}";
+                        });
+                        RecordsProgressBar.Invoke((MethodInvoker)delegate
+                        {
+                            RecordsProgressBar.PerformStep();
+                        });
+                    }
+                    i++;
+                }
+                catch (WebException ex)
+                {
+                    DialogResult result;
+                    lock (_lockerEnd)
+                    {
+                        result =
+                            MessageBox.Show(ex.Message, @"Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                    }
+                    switch (result)
+                    {
+                        case DialogResult.Cancel:
+                            return;
+                        case DialogResult.Retry:
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+            }
+            Interlocked.Decrement(ref _threadsCounter);
+        }
+
+        private void ThreadEndWaiting(object stateInfo)
+        {
+            while (_threadsCounter != 0)
+            {
+
+            }
+            Invoke((MethodInvoker) Close);
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            _close = true;
+            if (_threadsCounter != 0)
+            {
+                ThreadPool.QueueUserWorkItem(ThreadEndWaiting);
+                e.Cancel = true;
+            }
+            if (_dbConnection.State == ConnectionState.Open)
+                _dbConnection.Close();
+            //while (_threadsCounter != ThreadsNumber)
+            //{
+
+            //}
+            //lock (_lockerFile)
+            //{
+            //    _outputFile.Close();
+            //}
+        }
+    }
+}
